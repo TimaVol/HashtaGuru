64d542d681648b95297728215602a92704d22314
TimaVol <tima.voloshuk@gmail.com>
2023-03-03 18:26:10 +0200

    WIP

--- components/Solutions/Card.tsx
+++ components/Solutions/Card.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Arrow from "@/public/icons/arrow.svg";
+import Image, { StaticImageData } from "next/image";
 interface Props {
   subTitle: string;
   title: string;
   text: string;
-  icon: React.ReactElement;
+  img: StaticImageData;
   subTitleColor: string;
 }
 
@@ -12,27 +13,27 @@ export default function Card({
   subTitle,
   title,
   text,
-  icon,
+  img,
   subTitleColor,
 }: Props) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center md:flex-row md:even:flex-row-reverse">
       <div>
         <p
           className={`${subTitleColor} mb-[8px] font-semibold uppercase tracking-wide`}
         >
           {subTitle}
         </p>
-        <h3 className="mb-[14px]">{title}</h3>
-        <p className="mb-[30px]">{text}</p>
+        <h3 className="mb-[14px] text-black">{title}</h3>
+        <p className="mb-[30px] leading-6 text-darkGreen">{text}</p>
         <Link
           href={"#"}
-          className="flex w-max items-center text-[15px] text-darkGray"
+          className="mb-3 flex w-max items-center text-[15px] text-darkGray"
         >
           Learn More <Arrow className="ml-2" />
         </Link>
       </div>
-      {icon}
+      <Image src={img} alt={title} priority className="sm:w-1/2" />
     </div>
   );
 }
--- components/Solutions/index.tsx
+++ components/Solutions/index.tsx
@@ -1,39 +1,39 @@
 import Card from "./Card";
-import FirstCard from "@/public/icons/Solutions/card.svg";
-import SecondCard from "@/public/icons/Solutions/card2.svg";
-import ProfileIcon from "@/public/icons/Solutions/profile.svg";
+import firstCardImg from "@/public/images/Solutions/card1.png";
+import secondCardImg from "@/public/images/Solutions/card2.png";
+import profileImg from "@/public/images/Solutions/profile.png";
 
 const cards = [
   {
     title: "Influencer marketing",
     subTitle: "Influencer",
     text: "Create infinite pieces of content with our influencer product. Build,streamline,manage large-scale influencer programs",
-    icon: <FirstCard />,
+    img: firstCardImg,
     subTitleColor: "text-purpleLight",
   },
   {
     title: "Community marketing",
     subTitle: "Community",
     text: "Bring your relationships to new heights with our community product. Discover your biggest enthusiasts",
-    icon: <ProfileIcon />,
+    img: profileImg,
     subTitleColor: "text-yellow",
   },
   {
     title: "Managed services",
     subTitle: "services",
     text: "Our full-service solution lets our experts do the heavy lifting to optimize your community for brand building and storytelling",
-    icon: <SecondCard />,
+    img: secondCardImg,
     subTitleColor: "text-pink",
   },
 ];
 
 export default function Solutions() {
   return (
-    <div className="container">
+    <div className="container max-w-[789px] pt-[82px] pb-[30px]">
       <h2 className="family-poppins mb-[10px] text-center font-semibold text-black">
         Solutions for every need
       </h2>
-      <p className="mb-[85px] text-center text-darkGreen">
+      <p className="mb-[35px] text-center text-darkGreen">
         Join thousands of marketers and entrepreneurs for a 2-day event at the
         forefront of social commerce.
       </p>
@@ -44,7 +44,7 @@ export default function Solutions() {
           title={card.title}
           subTitle={card.subTitle}
           text={card.text}
-          icon={card.icon}
+          img={card.img}
           subTitleColor={card.subTitleColor}
         />
       ))}
