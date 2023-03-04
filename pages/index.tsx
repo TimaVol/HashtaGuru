6774d3aa93b7fac2a4aaaff4c03d111c109da229
TimaVol <tima.voloshuk@gmail.com>
2023-03-04 17:31:41 +0200

    done with Socials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import HappyClients from "@/components/HappyClients";
+import Socials from "@/components/Socials";
 import Solutions from "@/components/Solutions";
 import Statistics from "@/components/Statistics";
 import Subscribe from "@/components/Subscribe";
@@ -6,6 +7,7 @@ import Subscribe from "@/components/Subscribe";
 export default function Home() {
   return (
     <>
+      <Socials />
       <Solutions />
       <Statistics />
       <HappyClients />
